22cdaad133b198f1e3a72bd45af40a0077893e17
wrallee <wrallee@naver.com>
2020-04-26 03:46:13 +0900

    jpa examples

---
+++ src/test/java/com/wrallee/book/springboot/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,45 @@
+package com.wrallee.book.springboot.domain.posts;
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void articleSaveAndLoadList() {
+        // given
+        String title = "Title sample";
+        String content = "Contents sample";
+
+        postsRepository.save(Posts.builder()
+                                .title(title)
+                                .content(content)
+                                .author("wrallee@naver.com")
+                                .build());
+
+        // when
+        List<Posts> postsList = postsRepository.findAll();
+
+        // then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+}

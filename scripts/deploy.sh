7ce168ea1aba3d873c699c8ecbdbdf806cbded64
wrallee <wrallee@naver.com>
2020-05-28 01:02:44 +0900

    application examples - typos fix(nohub -> nohup)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -33,7 +33,7 @@ chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 
-nohub java -jar \
+nohup java -jar \
   -Dspring.config.location=classpath:/application.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
   -Dspring.profiles.active=real \
-  $JAR_NAME > $REPOSITORY/nohub.out 2>&1 &
+  $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &

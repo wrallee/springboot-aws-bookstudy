8f7959a053757f744205bc582a6058b887300c53
wrallee <wrallee@naver.com>
2020-05-29 22:22:05 +0900

    application examples - Server redundancy script typos fixed

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,13 +2,15 @@
 
 ABSPATH=$(readlink -f $0)
 ABSDIR=$(dirname $ABSPATH)
-source=${ABSDIR}/profile.sh
+source ${ABSDIR}/profile.sh
 
 REPOSITORY=/home/ec2-user/app/step3
 PROJECT_NAME=wrallee-springboot2-webservice
 
 echo "> Build 파일 복사"
 echo "> cp $REPOSITORY/zip/*.jar $REPOSITORY/"
+
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 새 애플리케이션 배포"
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
